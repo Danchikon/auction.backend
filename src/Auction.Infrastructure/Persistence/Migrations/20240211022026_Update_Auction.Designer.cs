1756565901a7635702ee2cbeb5ab946cafff46b7
Daniel Hrovinsky <d.hrovinsky@avidi.tech>
2024-02-11 03:23:27 +0100

    added migration for auction

--- src/Auction.Infrastructure/Persistence/Migrations/20240211022026_Update_Auction.Designer.cs
+++ src/Auction.Infrastructure/Persistence/Migrations/20240211022026_Update_Auction.Designer.cs
@@ -4,6 +4,7 @@ using Auction.Domain.Enums;
 using Auction.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Auction.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AuctionDbContext))]
-    partial class AuctionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240211022026_Update_Auction")]
+    partial class Update_Auction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
